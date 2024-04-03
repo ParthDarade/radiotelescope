76aa20b8a76b7403d3fa5ea29d8c1493209eb5fd
ParthDarade <parth.d.darade@gmail.com>
2024-04-03 15:14:41 +0530

    first commit note

--- plot.py
+++ plot.py
@@ -17,13 +17,14 @@ obs = {
     'ra_dec': '',
     'az_alt': ''
 }
-files = os.listdir('/home/pi/Desktop/radio_telescope/sweep_scan')
+files = os.listdir('/home/pi/Desktop/radio_telescope/Observatuion-dats')
 for file_name in files:
     print(file_name)
     if file_name.endswith('.dat'):
+        new_file_name = os.path.splitext(file_name)[0]
         virgo.plot(obs_parameters=obs, n=20, m=35, f_rest=1420.4057517667e6,
            vlsr=False, meta=False, avg_ylim=(-5,15), cal_ylim=(-20,260), cal_file='calibration.dat',
-           obs_file='/home/pi/Desktop/radio_telescope/sweep_scan/'+file_name, 
-           dB=True, spectra_csv='spectrum.csv', plot_file='/home/pi/Desktop/radio_telescope/plots/'+file_name+'plot.png')
+           obs_file='/home/pi/Desktop/radio_telescope/Observatuion-dats/'+file_name, 
+           dB=True, spectra_csv='/home/pi/Desktop/radio_telescope/spectraCSV/'+new_file_name+'.csv', plot_file='/home/pi/Desktop/radio_telescope/plots/'+file_name+'plot.png')
     
 print('done')
